e44b69ad399f2278cba04f557a8e6c493db14548
tiagompsimoes <tiago.simoes1500@gmail.com>
2025-04-23 21:05:50 +0100

    feat: esqueci-me-palavra-passe-frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 // import Perfil from './pages/Perfil.jsx';
 import Login from './pages/Login.jsx';
-
-import Header from './components/Header.jsx';
-// import Footer from './components/Footer.jsx';
-import Navbar from './components/Navbar.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
 
 import './assets/css/styles.css';
 
 function App() {
   return (
     <Router>
-      <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/perfil" element={<Perfil />} /> */}
           <Route path="/login" element={<Login />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
         </Routes>
-      {/* <Navbar /> */}
-      {/* <Footer /> */}
     </Router>
   );
 }
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Header from '../components/Header';
+import WatermarkLogin from '../components/WaterMarkLogin';
+import './ForgotPassword.css'; 
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+
+  const handleRecover = (e) => {
+    e.preventDefault();
+    console.log("Recuperar palavra passe para:", email);
+    // Backend
+  };
+
+  return (
+    <div className='main-container'>
+        <Header/>
+        <div className='content-wrapper'>
+          <div className="forgot-container">
+
+            <h2>Esqueci-me da palavra passe</h2>
+            <form onSubmit={handleRecover}>
+                <label htmlFor="email">Inserir email / Número de Aluno</label>
+                <input
+                    type="text"
+                    placeholder="Inserir email ou número de aluno"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <button type="submit">Recuperar</button>
+            </form>
+
+            <NavLink to="/login" className="link">
+                Login
+            </NavLink>
+
+            <div className='watermark'>
+            <WatermarkLogin/>
+            </div>
+          </div>
+        </div>
+    </div>
+
+  );
+};
+
+export default ForgotPassword;
